67f702f34b24fabcec2035aef9490427a31ba674
ZamTheCoder <rudhav935@gmail.com>
2024-12-26 15:29:23 +0530

    first commit

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+Alpha = input(("Enter Your Character: "))
+
+if ((Alpha >= "A" and Alpha <= "Z") or (Alpha >= "a" and Alpha <= "z")):
+    print("It is an Alphabet")
+
+else:
+    print("Its  not an Alphabet")
